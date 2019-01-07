0beaa3c7bb526e4c0ec34d0f8c7964904b144fc0
0h-n0 <kbu94982@gmail.com>
2019-01-07 12:10:11 +0900

    feat: update

--- bioplot/bioplot.py
+++ bioplot/bioplot.py
@@ -1,4 +1,7 @@
-import matplotlib
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 one_letter_to_three_letter = {
     'A' : 'ALA',
@@ -34,65 +37,100 @@ Amino Acid  Color      Triple        Amino Acid    Color  Triple
   ALA       dark grey  [200,200,200]   TRP         pink   [180,90,180]
   HIS       pale blue  [130,130,210]   PRO         flesh  [220,150,130]
 '''
-one_letter_aa_to_color = {
-    'A' : [200, 200, 200],
-    'R' : [20, 90, 255],
-    'N' : [0, 220, 220],
-    'D' : [230, 10, 10],
-    'B' : [230, 10, 10],
-    'C' : [230, 230, 0],
-    'E' : [230, 10, 10],
-    'Q' : [0, 220, 220],
-    'Z' : [0, 220, 220],
-    'G' : [235, 235, 235],
-    'H' : [130, 130, 210],
-    'I' : [15, 130, 15],
-    'L' : [15, 130, 15],
-    'K' : [20, 90, 255],
-    'M' : [230, 230, 0],
-    'F' : [50, 50, 170],
-    'P' : [220, 150, 130],
-    'S' : [250, 150, 0],
-    'T' : [250, 150, 0],
-    'W' : [180, 90, 180],
-    'Y' : [50, 50, 170],
-    'V' : [15, 130, 15],}
+one_letter_aa_to_index = {
+    'A' : 0,
+    'R' : 1,
+    'N' : 2,
+    'D' : 3,
+    'B' : 4,
+    'C' : 5,
+    'E' : 6,
+    'Q' : 7,
+    'Z' : 8,
+    'G' : 9,
+    'H' : 10,
+    'I' : 11,
+    'L' : 12,
+    'K' : 13,
+    'M' : 14,
+    'F' : 15,
+    'P' : 16,
+    'S' : 17,
+    'T' : 18,
+    'W' : 19,
+    'Y' : 20,
+    'V' : 12,}
 
+index_to_color = np.array(
+    [[200, 200, 200],
+     [20, 90, 255],
+     [0, 220, 220],
+     [230, 10, 10],
+     [230, 10, 10],
+     [230, 230, 0],
+     [230, 10, 10],
+     [0, 220, 220],
+     [0, 220, 220],
+     [235, 235, 235],
+     [130, 130, 210],
+     [15, 130, 15],
+     [15, 130, 15],
+     [20, 90, 255],
+     [230, 230, 0],
+     [50, 50, 170],
+     [220, 150, 130],
+     [250, 150, 0],
+     [250, 150, 0],
+     [180, 90, 180],
+     [50, 50, 170],
+     [15, 130, 15]]) / 255
 
 class Bioplot(object):
     def __init__(self, ax):
         self.ax = ax
+        self.set_colorbar()
 
-    def aaplot2d(self, seq: str, colors: list=None, autoscale: bool=True, max_width=25):
+    def grid(self, *args, **kwargs):
+        self.ax.grid(*args, **kwargs)
+        
+    def set_colorbar(self, cmap_name='bwr', vmin=None, vmax=None):
+        self.cmap_name = cmap_name
+        self.cmap = plt.get_cmap(cmap_name)
+        self.norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        self.scalarMap = cm.ScalarMappable(norm=self.norm, cmap=self.cmap)
+
+    def get_rgb(self, val):
+        return self.scalarMap.to_rgba(val)
+        
+    def aaplot2d(self, seq: str, colors: list=None, autoscale: bool=True, max_width=50):
         aa_length = len(seq)
         height = aa_length // max_width + 1
         left = np.zeros(height)
-        for idx, s in enumerate(seq):
+        h_range = list(range(height))
+        
+        if isinstance(colors, list):
+            colors = np.array(colors)
+
+        for idx in range(max_width):
+            column_indexes = np.array(np.arange(idx, aa_length, max_width))
             if colors is None:
-                c = np.array(one_letter_aa_to_color[s]) / 255
+                c = index_to_color[[one_letter_aa_to_index[seq[i]] for i in column_indexes]]
             else:
-                c = colors[idx]
+                c = self.get_rgb(colors[column_indexes])
                 
             _y = np.zeros(height)
+            for i in range(c.shape[0]):
+                _y[i] = 1
+            self.ax.barh(h_range, _y, 0.9, left=left, color=c, label="a")
+            left += _y
 
-            _y = - ((idx + 1)// (max_width) + 1)
-            print(_y, idx, idx // max_width, left)
-            self.ax.barh(_y, 0.8, left=left, color=c)
-            left[idx // (max_width-1)] += 1            
+        for h in h_range:
+            for i in range(max_width):
+                idx = h*max_width+i
+                if aa_length <= idx:
+                    break
+                ax.text(i+0.5, h, seq[idx], family='monospace',
+                        horizontalalignment='center', verticalalignment='center')
 
     def aaplot3d(self):
-        pass
-    
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    import numpy as np
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    bplot = Bioplot(ax)
-    seq = "AABBCC"
-    seq = "MEKEKKVKYFLRKSAFGLASVSAAFLVGSTVFAVDSPIEDTPIIRNGGELTNLLGNSETTLALRNEESATADLTAAAVADTVAAAAAENAGAAAWEAAAAADALAKAKADALKEFNKYGVSDYYKNLINNAKTVEGVKDLQAQVVESAKKARISEATDGLSDFLKSQTPAEDTVKSIELAEAKVLANRELDKYGVSDYHKNLINNAKTVEGVKDLQAQVVESAKKARISEATDGLSDFLKSQTPAEDTVKSIELAEAKVLANRELDKYGVSDYYKNLINNAKTVEGVKALIDEILAALPKTDTYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTEKPEVIDASELTPAVTTYKLVINGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTEKPEVIDASELTPAVTTYKLVINGKTLKGETTTKAVDAETAEKAFKQYANDNGVDGVWTYDDATKTFTVTEMVTEVPGDAPTEPEKPEASIPLVPLTPATPIAKDDAKKDDTKKEDAKKPEAKKEDAKKAETLPTTGEGSNPFFTAAALAVMAGAGALAVASKRKED"
-    seq = "MEKEKKVKYFLRKSAFGLASVSAAFLVGSTVFAVDSPIEDTPIIRNGGELTNLL"
-    color = [0.1, 0.2, 0.3, 0.1, 0.2, 0.3]
-    bplot.aaplot2d(seq)
-    plt.show()
+        raise NotImplementedError
